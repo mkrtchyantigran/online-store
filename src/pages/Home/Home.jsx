5a4a81f5991ab0dda3e0507efca47ea41ba0cf76
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-03-29 23:45:07 +0200

    import ShopByCategories and NewProducts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 
 import './home.scss'
 import MainBanner from '../../components/MainBanner/MainBanner'
-import ShopByCategories from '../../components/ShopByCategories/ShopByCategories'
+import ShopByCategories from '../../components/ShopByCategories/ShopByCategories';
+import NewProducts from '../../components/newProducts/NewProducts';
+
 export default function Home() {
     return (
         <main>
             <MainBanner />
             <ShopByCategories />
+            <NewProducts />
         </main>
     )
 }
