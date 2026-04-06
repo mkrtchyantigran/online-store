8a87dbf4ed8d4a40870612c18e9f1db85b978ed9
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-04-06 17:33:13 +0200

    add ShopByRoom section for Home page

---
+++ src/components/ShopByRoom/ShopByRoom.jsx
@@ -0,0 +1,65 @@
+
+import arrowRightIcon from "../../assets/icons/arrow-right.svg"
+import BedRoomImg from "../../assets/images/shop-by-room/modern-bedroom.jpg"
+import LivinRoomImg from "../../assets/images/shop-by-room/modern-living-room.jpg"
+import HallwayImg from "../../assets/images/shop-by-room/modern-hallway.jpg"
+import KitchenImg from "../../assets/images/shop-by-room/modern-kitchen.jpg"
+
+import "./ShopByRoom.scss";
+
+export default function ShopByRoom() {
+  return (
+    <div className='shop-by-room'>
+        <div className="container">
+           
+           <ul className="products-section-cards">
+                <li className='section-card'>
+                    <a 
+                        href="#"
+                    >
+                        <span className='with-arrow-right'>
+                            Bedroom
+                            <img src={arrowRightIcon} alt="arrow-right-icon" />
+                        </span>
+                    </a>
+                    <img src={BedRoomImg} alt="bedroom" />
+                </li>
+                <li className='section-card'>
+                    <a 
+                        href="#"
+                    >
+                        <span className='with-arrow-right'>
+                            Living room
+                            <img src={arrowRightIcon} alt="arrow-right-icon" />
+                        </span>
+                    </a>
+                    <img src={LivinRoomImg} alt="living-room" />
+                </li>
+                <li className='section-card'>
+                    <a 
+                        href="#"
+                    >
+                        <span className='with-arrow-right'>
+                            Hallway
+                            <img src={arrowRightIcon} alt="arrow-right-icon" />
+                        </span>
+                    </a>
+                    <img src={HallwayImg} alt="hallway" />
+                </li>
+                <li className='section-card'>
+                    <a 
+                        href="#"
+                    >
+                        <span className='with-arrow-right'>
+                            Kitchen
+                            <img src={arrowRightIcon} alt="arrow-right-icon" />
+                        </span>
+                    </a>
+                    <img src={KitchenImg} alt="kitchen" />
+                </li>
+           </ul>
+        </div>
+    </div>
+  )
+}
+
