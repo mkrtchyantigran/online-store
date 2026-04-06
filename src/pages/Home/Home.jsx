da39fe7b827f6fba88f87015e4a8d615d123e9e1
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-04-06 17:48:53 +0200

    update Home page, import components ShopByRoom and PromoVideo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './home.scss'
 import MainBanner from '../../components/MainBanner/MainBanner'
 import ShopByCategories from '../../components/ShopByCategories/ShopByCategories';
 import NewProducts from '../../components/newProducts/NewProducts';
+import ShopByRoom from '../../components/ShopByRoom/ShopByRoom';
+import PromoVideo from '../../components/PromoVideo/PromoVideo';
 
 export default function Home() {
     return (
@@ -10,6 +12,8 @@ export default function Home() {
             <MainBanner />
             <ShopByCategories />
             <NewProducts />
+            <ShopByRoom />
+            <PromoVideo />  
         </main>
     )
 }
