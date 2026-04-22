6fd53aa3727474786734e3db5ac263607b09b32e
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-04-22 18:48:54 +0200

    import BrowserRouter, Routes, Route, lazy, Suspense and use it for main App function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import Home from "../src/pages/Home/Home";
-import Header from "../src/components/Header/Header.jsx";
+
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+const Header = lazy(() => import("./components/Header/Header"));
+const Home = lazy(() => import("./pages/Home/Home"));
+const ShopePage = lazy(() => import("./pages/Shop/Shop.jsx"));
+
 
 export default function App () {
 
 
   return (
-      <div className="app">
-        <Header />
-        <Home />
-      </div>
+     <BrowserRouter>
+       <Suspense fallback={<div>Loading...</div>}>
+         <div className="app">
+          <Header />
+          <main>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/shop' element={<ShopePage />} />
+            </Routes>
+          </main>
+        </div>
+       </Suspense>
+     </BrowserRouter>
     );
 }
