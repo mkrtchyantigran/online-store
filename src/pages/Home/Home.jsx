1cc631b19140043609dac6b93528710c69081d33
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-04-22 18:51:13 +0200

    modified Home page add IconsBar and Footer components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,9 @@ import NewProducts from '../../components/newProducts/NewProducts';
 import ShopByRoom from '../../components/ShopByRoom/ShopByRoom';
 import PromoVideo from '../../components/PromoVideo/PromoVideo';
 import Bestsellers from '../../components/Bestsellers/Bestsellers';
+import CustomerReviews from '../../components/CustomerReviews/CustomerReviews';
+import IconsBar from '../../components/IconsBar/IconsBar';
+import Footer from '../../components/Footer/Footer';
 
 export default function Home() {
     return (
@@ -16,6 +19,9 @@ export default function Home() {
             <ShopByRoom />
             <PromoVideo />  
             <Bestsellers />
+            <CustomerReviews />
+            <IconsBar />
+            <Footer />
         </main>
     )
 }
